83c4a83135d324021b824e982f0ac6fffdf02178
Michell D'Enjoy <166136472+michelldenjoy@users.noreply.github.com>
2024-07-07 18:03:12 +0000

    generador de palabras aleatorias

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function generateRandomCard() {
+  // declaracion de arrays con los valores de las cartas
+  const valor = ["2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"];
+  const symbol = ["&hearts;", "&diams;", "&clubs;", "&spades;"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  //funcion que genera el valor y el simbolo aleatorio
+  const randomValor = valor[Math.floor(Math.random() * valor.length)];
+  const randomSymbol = symbol[Math.floor(Math.random() * symbol.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  //declaracion de constantes para guardar el valor de la funcion por medio del ID
+  //si declaro esto así, como se muestra entre los comentarios de la linea 12, 13 y 14 el condicional no me recocnoce el color. ¿por qué?
+  const symbolTop = document.getElementById("symbolTop"); //.innerHTML = randomSymbol;
+  const cardValue = document.getElementById("value"); //.innerHTML = randomValor;
+  const symbolDown = document.getElementById("symbolDown"); //.innerHTML = randomSymbol;
+
+  //en cambio si lo declaro así, el condicional si me reconoce el color ¿por qué?
+  symbolTop.innerHTML = randomSymbol;
+  cardValue.innerHTML = randomValor;
+  symbolDown.innerHTML = randomSymbol;
+
+  if (randomSymbol === "&hearts;" || randomSymbol === "&diams;") {
+    symbolTop.style.color = "red";
+    cardValue.style.color = "red";
+    symbolDown.style.color = "red";
+  } else {
+    symbolTop.style.color = "black";
+    cardValue.style.color = "black";
+    symbolDown.style.color = "black";
+  }
+}
+
+window.onload = generateRandomCard();
+setInterval(generateRandomCard, 10000);
